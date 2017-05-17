24c3b95203bfbd6fcc0318c0eb65764193ff4399
Cole Abbeduto <abbedutocg@msoe.edu>
2017-05-17 10:49:03 -0500

    Messed with the chart some more and added a navigation tab thingy at the top of the page

--- src/client/Client.js
+++ src/client/Client.js
@@ -2,31 +2,91 @@
  * Created by cole2 on 5/15/2017.
  */
 
-//import * as io from "socket.io/lib/client";
-const angularApp = angular.module('angularApp', ['ngMaterial']);
-//let socket = io();
+const angularApp = angular.module('angularApp', ['ngMaterial', 'chart.js']);
 
-angularApp.controller('angularController', function($scope, socket) {
+angularApp.controller('angularController', function ($scope,socket) {
     "use strict";
 
-    socket.on("newData", function(newData){
+    $scope.defaultData = [[],[],[],[]];
+    $scope.currentNavItem = 'page1';
+
+    $scope.beginDataFetch = function() {
+        $scope.data = $scope.defaultData;
+        $scope.labels = [];
+
+        $scope.dataFetch = true;
+        socket.on("newData", parseData);
+    };
+
+    function parseData(newData) {
         if (newData !== null && newData !== undefined) {
             newData = JSON.parse(newData);
 
-            if (Array.isArray(newData))
-                $scope.data += "[" + newData + "]\n";
+            if (Array.isArray(newData)) {
+                addValuesToGraph(newData);
+            }
         }
-    });
+    }
 
+    $scope.stopDataFetch = function(){
+        $scope.dataFetch = false;
+        $scope.data = [[],[],[],[]];
+        $scope.labels = [];
+        socket.removeListener("newData", parseData);
+    };
 
-    $scope.$watch('checkingBox', function(value){
-        socket.emit("newData", value);
-    });
+    $scope.addRand = function () {
+        "use strict";
+        addValuesToGraph([Math.random(), Math.random(), Math.random(), Math.random()]);
+    };
+
+    $scope.labels = [];
+    $scope.series = ["Num 1", "Num 2", "Num 3", "Num 4"];
+
+    $scope.options = {
+        scales: {
+            yAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Values'
+                }
+            }],
+            xAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Time (HH:MM:SS)'
+                }
+            }]
+        },
+        elements : {
+            line: {
+                tension: 0
+            }
+        },
+        responsive: false,
+        animation: false,
+    };
+
+    function addValuesToGraph(newData) {
+        "use strict";
+        if ($scope.data[0].length > 20) {
+            /*$scope.data.forEach(function(array){
+                array.shift();
+            });
+            $scope.labels.shift();*/
+        }
+
+        $scope.data.forEach(function(array, index){
+            array.push(newData[index]);
+        });
+        $scope.labels.push(new Date().toLocaleTimeString());
+    }
 
 
 });
 
 angularApp.factory('socket', function ($rootScope) {
+    "use strict";
     let socket = io.connect();
     return {
         on: function (eventName, callback) {
